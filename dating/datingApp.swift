c422e523d2f07f7b57b7ab5695dce347aced416d
kehindeBankole <bankolek1@gmail.com>
2024-02-19 10:57:45 +0100

    Initial Commit

---
+++ dating/datingApp.swift
@@ -0,0 +1,17 @@
+//
+//  datingApp.swift
+//  dating
+//
+//  Created by kehinde on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct datingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
